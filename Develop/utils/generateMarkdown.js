0cdae863445046ce97cc5e72373a618de67c5677
Alex <alexmore080@gmail.com>
2022-09-07 13:23:20 -0500

    changed badge to check if array value exists at all

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,17 +1,37 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const MIT = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) "
+const MIT = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) ";
+const GNU = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0) ";
+const ISC = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC) ";
+const Apache = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) ";
 
 
 function renderLicenseBadge(license) {
-	console.log(license[0])
-	if(license[0] === "MIT" && license[1] === "GNU" && license[2] === "ISC" && license[3] === "Apache"){
-		return MIT + "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0) " + "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC) " + "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) "
-	} else if(license[0] === "MIT" && license[1] === "GNU" && license[2] === "ISC"){
-		return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) " + "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0) " + "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC) ";
-	} else if(license[0] === "MIT" && license[1] === "GNU"){
-		return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) " + "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0) "
+	console.log(license)
+	let badgeArr = [];
+
+	if(license.includes("MIT") === true){
+		badgeArr.push(MIT)
 	}
+	if(license.includes("GNU") === true){
+		badgeArr.push(GNU)
+	}
+	if(license.includes("ISC") === true){
+		badgeArr.push(ISC)
+	}
+	if(license.includes("Apache") === true){
+		badgeArr.push(Apache)
+	}
+	
+	return badgeArr.splice(",");
+
+	// if(license[0] === "MIT" && license[1] === "GNU" && license[2] === "ISC" && license[3] === "Apache"){
+	// 	return MIT + "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0) " + "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC) " + "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) "
+	// } else if(license[0] === "MIT" && license[1] === "GNU" && license[2] === "ISC"){
+	// 	return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) " + "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0) " + "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC) ";
+	// } else if(license[0] === "MIT" && license[1] === "GNU"){
+	// 	return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) " + "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0) "
+	// }
 }
 
 // TODO: Create a function that returns the license link
@@ -20,11 +40,23 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+	let licenseText = "Licensed under the "
+	for(let x of license){
+		console.log(license.length)
+		if(license.length > 1){
+			licenseText += license + ","
+		} else{
+			licenseText += x;
+		}
+		return licenseText;
+	}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 ## Usage
 ${data.usage}
 ## Features
@@ -32,7 +64,7 @@ ${data.features}
 ## Installation
 ${data.installation}
 ## License
-${renderLicenseBadge(data.license)}
+${renderLicenseSection(data.license)}
 `;
 }
 
